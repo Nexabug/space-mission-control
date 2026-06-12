39396df1c913c72b2a37c67b97b526c9c07e4188
Nexabug <fusionflarepath@gmail.com>
2026-06-12 08:47:51 +0530

    build the staics page and some more improvment

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/homepage/Homepage";
 import Missionpage from "./pages/missionpage/Missionpage";
@@ -7,20 +7,44 @@ import Crew from "./pages/crewpage/Crew";
 import Navbar from "./components/Navbar";
 import Applayout from "./Applayout";
 import "./App.css";
+
 function App() {
+  const [persent, setpersentage] = useState(0);
+  useEffect(() => {
+    function handleScroll() {
+      const scrolltop = window.scrollY;
+      const documentHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+
+      const percentage = (scrolltop / documentHeight) * 100;
+
+      setpersentage(percentage);
+    }
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="body">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Applayout />}>
-            <Route index element={<Homepage />} />
-            <Route path="/mission" element={<Missionpage />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/crew" element={<Crew />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <>
+      <div className="progress" style={{ width: `${persent}%` }}></div>
+
+      <div className="body">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Applayout />}>
+              <Route index element={<Homepage />} />
+              <Route path="/mission" element={<Missionpage />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/crew" element={<Crew />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
   );
 }
 
--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -2,32 +2,146 @@ import React from "react";
 import Btn from "../../Ui/Btn";
 import "./homepage.css";
 import { Link } from "react-router-dom";
+import { mission, crew, planedmission, rocket } from "../../../data";
 
 function Homepage() {
+  const TotalMissions = String(mission.length + planedmission.length).padStart(
+    2,
+    "0",
+  );
+
+  const TotalCrew = String(crew.length).padStart(2, "0");
+
+  const UpcomingLaunches = String(planedmission.length).padStart(2, "0");
+
+  const TotalRockets = String(rocket.length).padStart(2, "0");
+
+  const succesfullLaunches = String(
+    mission.filter((i) => i.status === "Success").length,
+  ).padStart(2, "0");
   return (
-    <div className="home">
-      <div className="home-content">
-        <h1>
-          <em>space mission</em> <br />
-          control
-        </h1>
-        <p>
-          its a space mission control site for all the info about the space
-          related things
-        </p>
-        <div className="btns">
-          <Link to="/mission">
-            <Btn>see details</Btn>
-          </Link>
-          <Link to="/dashboard">
-            <Btn bg="transparent"> dashboard </Btn>
-          </Link>
+    <>
+      <div className="home">
+        <div className="home-content">
+          <h1>
+            <em>space mission</em> <br />
+            control
+          </h1>
+          <p>
+            its a space mission control site for all the info about the space
+            related things
+          </p>
+          <div className="btns">
+            <Link to="/mission">
+              <Btn>see details</Btn>
+            </Link>
+            <Link to="/dashboard">
+              <Btn bg="transparent"> dashboard </Btn>
+            </Link>
+          </div>
+        </div>
+        <div className="hero-img">
+          <img src="/rocket.png" alt="" />
         </div>
       </div>
-      <div className="hero-img">
-        <img src="/rocket.png" alt="" />
+
+      <div className="stats">
+        <h1>Statistics</h1>
+
+        <div className="stats-container">
+          <div className="stats-card">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z"
+              />
+            </svg>
+            <p className="stats-detail">{TotalMissions}</p>
+            <p>total missions</p>
+          </div>
+          <div className="stats-card">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z"
+              />
+            </svg>
+            <p className="stats-detail">{succesfullLaunches}</p>
+            <p>succesfull launches</p>
+          </div>
+          <div className="stats-card">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5"
+              />
+            </svg>
+            <p className="stats-detail">{UpcomingLaunches}</p>
+            <p>upcoming launches</p>
+          </div>
+          <div className="stats-card">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.59 14.37a6 6 0 0 1-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 0 0 6.16-12.12A14.98 14.98 0 0 0 9.631 8.41m5.96 5.96a14.926 14.926 0 0 1-5.841 2.58m-.119-8.54a6 6 0 0 0-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 0 0-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 0 1-2.448-2.448 14.9 14.9 0 0 1 .06-.312m-2.24 2.39a4.493 4.493 0 0 0-1.757 4.306 4.493 4.493 0 0 0 4.306-1.758M16.5 9a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z"
+              />
+            </svg>
+            <p className="stats-detail">{TotalRockets}</p>
+            <p>total rokets</p>
+          </div>
+          <div className="stats-card">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+              />
+            </svg>
+            <p className="stats-detail">{TotalCrew}</p>
+            <p>total crew memebers</p>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
